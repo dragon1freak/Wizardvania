4047215cebd7d81f82f1b9bb7c38fb5d860091ec
James Ritchey <jim0ritchey@gmail.com>
2022-03-15 13:25:00 -0400

    Final jam submission, small bugs fixed

--- Scripts/Indicator.gd
+++ Scripts/Indicator.gd
@@ -4,6 +4,13 @@ var current_input : String = "keyboard"
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	InputHelper.connect("device_changed", self, "_on_input_device_changed")
+	current_input = InputHelper.guess_device_name()
+	if InputHelper.guess_device_name() == "keyboard":
+		$KeyboardSprite.visible = true
+		$ConsoleSprite.visible = false
+	else:
+		$KeyboardSprite.visible = false
+		$ConsoleSprite.visible = true
 
 func _on_input_device_changed(device : String, device_index : int) -> void:
 	if device == "keyboard":
